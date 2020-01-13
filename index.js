7a14f2920950acc6bcaabd57bc7d7b4211eac071
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-01-13 01:54:37 -0500

    Template Literals

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+
+/*Template literals are string literals allowing embedded expressions.
+Template literals are enclosed by the back-tick (` `)instead of double or single quotes. 
+Template literals can contain placeholders. 
+These are indicated by the dollar sign and curly braces (${expression}). 
+*/
+//Example
+let name="Jon Snow";
+let age=25;
+console.log(`Welcome,${name}, you don't look like a day over ${age-5}`);
